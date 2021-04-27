b9abbc4ea83bec4f8f5df534092240b0e2a8fe17
Planet Farm <contact@planetfarm.finance>
2021-04-27 11:44:51 +0200

    Fix buttons background color

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2858,10 +2858,10 @@ var baseColors = {
 var brandColors = {
     binance: "#F0B90B",
 };
-var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#FAF9FA", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", tertiary: "#EFF4F5", text: "#452A7A", textDisabled: "#BDC2C4", textSubtle: "#8f80ba", borderColor: "#E9EAEB", card: "#FFFFFF", icon: "#FFFFFF", gradients: {
+var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#FAF9FA", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", tertiary: "#EFF4F5", text: "#452A7A", textDisabled: "#BDC2C4", textSubtle: "#557ded", borderColor: "#E9EAEB", card: "#FFFFFF", icon: "#FFFFFF", gradients: {
         bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
     } });
-var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#fc5130", background: "#0e121b", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#eeeaf4", primaryDark: "#0098A1", tertiary: "#1c2335", text: "#557ded", textDisabled: "#1c2335", textSubtle: "#557ded", borderColor: "#524B63", card: "#050401", icon: "#557ded", gradients: {
+var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#fc5130", background: "#0e121b", backgroundDisabled: "#afb9cd", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#FFFFFF", primaryDark: "#0098A1", tertiary: "#1c2335", text: "#557ded", textDisabled: "#1c2335", textSubtle: "#557ded", borderColor: "#524B63", card: "#050401", icon: "#557ded", gradients: {
         bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
     } });
 
